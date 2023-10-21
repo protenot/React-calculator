cacb3270742ecd4b291a0b802be20f0210752e7c
Olga_Belaya <protenot@yandex.ru>
2023-10-21 18:19:08 +0300

    for deploy

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p className="header-title">
-          Calculator
+          Simple Calculator
         </p>
         <div className="navigation">
           <Link to="/">Main page</Link>
