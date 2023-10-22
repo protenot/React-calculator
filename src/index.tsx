590ed5c499534caf2d74fef855937e7a6d3bdf08
Olga_Belaya <protenot@yandex.ru>
2023-10-22 16:20:26 +0300

    install prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-// import AppRouterV5Lazy from './apps/AppRouterV5Lazy';
-// import AppRouterV5 from './apps/AppRouterV5';
-// import AppHistory from './apps/AppHistory';
 import App from "./App";
-//import './index.css';
+
 
 const basename = process.env.CONTENT_ROOT;
 
